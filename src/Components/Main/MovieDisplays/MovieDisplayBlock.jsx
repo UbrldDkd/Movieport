8c5a310473dbe66587249adfd3a2ca1bb6fa4467
dkd <lukaface98@gmail.com>
2025-08-03 15:16:07 -0700

    working out routing

--- src/Components/Main/MovieDisplays/MovieDisplayBlock.jsx
+++ src/Components/Main/MovieDisplays/MovieDisplayBlock.jsx
@@ -1,13 +1,13 @@
 import MovieCard from './MovieCard';
 import { Keys } from '../../Keys.js';
 
-export default function MovieDisplayBlock({ movies }) {
+export default function MovieDisplayBlock({ fullContent, type }) {
   return (
     <div className="w-full py-10">
       <div className="flex flex-wrap gap-4">
-        {movies.map((movie) => (
-          <div key={movie[Keys.id]} className="w-[166px]">
-            <MovieCard movie={movie} />
+        {fullContent.map((item) => (
+          <div key={item[Keys.id]} className="w-[166px]">
+            <MovieCard content={item} type={type} />
           </div>
         ))}
       </div>
--- src/Components/Main/Pagination/PaginationPanel.jsx
+++ src/Components/Main/Pagination/PaginationPanel.jsx
@@ -1,10 +1,13 @@
 import PaginationBar from "./PaginationBar";
 
 export default function PaginationPanel({ totalPages, currentPage, onPageChange }) {
+
+  const pagesToDisplay = Math.min(totalPages, 10);
+
   return (
     <div className="flex justify-center space-x-2 mt-4 w-24 mx-auto">
 
-      {Array.from({ length: totalPages }, (_, i) => (
+      {Array.from({ length: pagesToDisplay }, (_, i) => (
 
         <PaginationBar
           key={i}
