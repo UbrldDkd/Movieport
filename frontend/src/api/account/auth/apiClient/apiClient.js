0ac956e259f60fafa3896e6a0fd78d23bd832c17
dkd <lukaface98@gmail.com>
2026-08-02 00:37:07 +0200

    feat: tuned enviroment variables for fullstack setup

--- frontend/src/api/account/auth/apiClient/apiClient.js
+++ frontend/src/api/account/auth/apiClient/apiClient.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
+console.log('API URL:', import.meta.env.VITE_API_URL);
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
