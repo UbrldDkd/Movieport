90892372c0a13b9e2a0dab1f725ff0ab4712b59f
dkd <lukaface98@gmail.com>
2026-08-02 03:07:05 +0200

    feat: finishing final details for deployment

--- backend/Movieport/Movieport/wsgi.py
+++ backend/Movieport/Movieport/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Movieport.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Movieport.settings')
 
 application = get_wsgi_application()
